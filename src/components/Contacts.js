627e6214421b0cf9639069fd07079c621f840be0
geekwims <suyoung154@gmail.com>
2017-02-27 12:56:22 +0900

    I studied about immutability helper and how to add/remove/modify element to array.

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import ContactInfo from './ContactInfo'
+import ContactInfo from './ContactInfo';
+import update from 'react-addons-update';
+import ContactCreator from './ContactCreator';
 
 class Contacts extends React.Component {
   constructor(props) {
@@ -10,8 +12,41 @@ class Contacts extends React.Component {
         {name: "Betty", phone: "010-0000-0002"},
         {name: "Charlie", phone: "010-0000-0003"},
         {name: "David", phone: "010-0000-0004"}
-      ]
+      ],
+      selectedKey: -1
     };
+  }
+
+  _insertContact(name, phone) {
+    let newState = update(this.state, {
+      contactData: {
+        $push: [{"name": name, "phone": phone}]
+      }
+    });
+    this.setState(newState);
+  }
+
+  _onSelect(key) {
+    if (key == this.state.selectedKey) {
+      console.log("key selection is cancelled");
+      this.setState({
+        selectedKey: -1
+      });
+      return;
+    }
+
+    this.setState({
+      selectedKey: key
+    });
+    console.log(key + " is selected");
+  }
+
+  _isSelected(key) {
+    if (this.state.selectedKey == key) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   render() {
@@ -21,10 +56,14 @@ class Contacts extends React.Component {
         <ul>
           {this.state.contactData.map((contact, i) => {
             return (
-              <ContactInfo name={contact.name} phone={contact.phone} key={i}/>
+              <ContactInfo name={contact.name} phone={contact.phone} key={i}
+                contactKey={i}
+                isSelected={this._isSelected.bind(this)(i)}
+                onSelect={this._onSelect.bind(this)}/>
             )
           })}
         </ul>
+        <ContactCreator onInsert={this._insertContact.bind(this)}/>
       </div>
     );
   }
